d7ed0da268a5d16543ab07570203394df84c5319
lestherll <qweqwe123.aa690@gmail.com>
2022-02-03 14:41:18 +0000

    Add rest of chapter 2 exercises

--- src/main/scala/Chapter2.scala
+++ src/main/scala/Chapter2.scala
@@ -48,4 +48,16 @@ object Chapter2 {
     loop(0)
   }
 
+  // Exercise 2.3 - Currying
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) =
+    a => b => f(a, b)
+
+  // Exercise 2.4 - Uncurry
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C =
+    (a, b) => f(a)(b)
+
+  // Exercise 2.5
+  def compose[A, B, C](f: B => C, g: A => B): A => C =
+    a => f(g(a))
+
 }
