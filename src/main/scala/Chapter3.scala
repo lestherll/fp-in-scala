6fc5c43847f85f6a58e5d9181bc9a7ba49934786
lestherll <qweqwe123.aa690@gmail.com>
2022-02-04 11:52:22 +0000

    Add Exercise 3.4 and .5 solution

--- src/main/scala/Chapter3.scala
+++ src/main/scala/Chapter3.scala
@@ -11,7 +11,30 @@ object Chapter3 {
     // Exercise 3.2
     def tail[A](list: List[A]) = list match {
       case Nil => Nil
-      case Cons(h, t) => t
+      case Cons(_, t) => t
+    }
+
+    // Exercise 3.3
+    def setHead[A](l: List[A], nh: A) = l match {
+      case Cons(_, t) => Cons(nh, t)
+      case Nil => sys.error("setting head on empty list")
+    }
+
+    // Exercise 3.4 - generalising tail
+    def drop[A](l: List[A], n: Int): List[A] = {
+      if (n <= 0) l
+      else l match {
+        case Nil => Nil
+        case Cons(_, t) => drop(t, n-1)
+      }
+    }
+
+    // Exercise 3.5 - dropWhile
+    def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+      case Nil => Nil
+      case Cons(head, tail) => 
+        if (f(head)) dropWhile(tail, f)
+        else l
     }
 
     def sum(ints: List[Int]): Int = ints match {
--- src/test/scala/Chapter3Test.scala
+++ src/test/scala/Chapter3Test.scala
@@ -35,4 +35,24 @@ class Chapter3Test extends AnyFlatSpec {
     assert(List.tail(list_with_string) == List.tail(list_with_int))
   }
 
+  "setHead" should "replace the head value of the list" in {
+    val l = List("abc", "def")
+    val new_list = List.setHead(l, "ghi")
+    assert(new_list != l)
+  }
+
+  "List.drop with n=1" should " equal to List.tail" in {
+    val l = List(1, 2, 3, 4, 5)
+    assert(List.drop(l, 1) == List.tail(l))
+  }
+
+  "List.drop" should "drop first n values" in {
+    val l = List(1, 2, 3, 4, 5)
+    assert(List.drop(l, 3) == List(4, 5))
+  }
+
+  "List.drop while" should "drop values as long as the predicate is matched" in {
+    val l = List(1, 2, 3, 4, 5)
+    assert(List.dropWhile(l, (x: Int) => x < 3) == List(3, 4, 5))
+  }
 }
